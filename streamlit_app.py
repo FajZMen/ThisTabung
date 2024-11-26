17d08a55c16df16b561038f9f7a1699927629174
FajZMen <fajarzman52@gmail.com>
2024-11-26 16:07:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Welcome to Tabung Gajelas")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Di web ini anda bisa Menghitung Tabung (VERY COOL!)."
 )
