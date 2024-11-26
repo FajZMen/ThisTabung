27d3848d13bc8dd05ac0c1d283237abfc79e8cf8
FajZMen <fajarzman52@gmail.com>
2024-11-26 16:10:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("Welcome to Tabung Gajelas")
 st.write(
     "Di web ini anda bisa Menghitung Tabung (VERY COOL!)."
 )
+
+jarijari = st.number_input("Masukan jari-jari tabung: ",0)
+panjang = st.number_input("Masukan panjang tabung: ",0)
