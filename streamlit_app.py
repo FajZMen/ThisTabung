04f65dba185f9bcc52d811c60339e461d778006e
FajZMen <fajarzman52@gmail.com>
2024-11-26 16:27:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,14 @@ st.write(
 jarijari = st.number_input("Masukan jari-jari tabung: ",0)
 panjang = st.number_input("Masukan panjang tabung: ",0)
 justload = "Processing your Tabung :P"
+almostload = "Almost done!"
 
 if st.button("Hitung Volume", type="primary"):
     processing = st.progress(0, text=justload)
     for percent in range(100):
         wait(0.01)
+        if percent > 80:
+            processing.progress(percent + 1, text=almostload)
         processing.progress(percent + 1, text=justload)
     wait(1)
     processing.empty()
